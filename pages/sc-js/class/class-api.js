e808bf8c377f83560aa89ac93cd4db8b0199bead
guoyansai <guoyansai@qq.com>
2022-08-02 08:18:14 +0800

    API封装2

--- pages/sc-js/class/class-api.js
+++ pages/sc-js/class/class-api.js
@@ -1,34 +1,26 @@
 export default class {
 	constructor(jsonConf) {
-		this.jsonConf = jsonConf
+		this.jsonConf = jsonConf;
+		this.isUni = jsonConf.type === 'uni'
 	}
 	scApi(vUrl, vParams, vConfig) {
-		this.loadShow({
+		const [url, params, config] = [this.scUrl(vUrl), this.scParams(vParams), this.scConfig(vConfig)]
+		this.scLoadShow({
 			title: "数据加载中",
 		});
-		const [url, params, config] = [this.scUrl(vUrl), this.scParams(vParams), this.scConfig(vConfig)]
 		console.log(666.9, url, params, config)
 		return new Promise((resolve, reject) => {
-			if (this.jsonConf.type === 'uni') {
-				uni.request({
-					url: url,
-					data: params,
-					method: config.method || 'POST', // 'GET'||'POST'
-					header: config.header,
-					timeout: config.timeout || this.jsonConf.timeout,
-					success: (res) => {
-						this.msgShow('加载成功')
-						console.log(res.data);
-						resolve(res.data);
-					},
-					fail: (err) => {
-						this.msgShow('失败了')
-						reject(err)
-					},
-					complete: () => {
-						this.loadClose()
-					}
-				});
+			if (this.isUni) {
+				this.uniApi(url, params, config).then(res => {
+					this.scMsgShow('加载成功')
+					console.log(res.data);
+					resolve(res.data);
+				}).catch(err => {
+					this.scMsgShow('失败了')
+					reject(err)
+				}).finally(() => {
+					this.scLoadClose()
+				})
 			} else {
 				// axois
 			}
@@ -47,20 +39,69 @@ export default class {
 		// vConfig.header['Content-Type']='application/json'
 		return vConfig
 	}
+	scLoadShow(obj) {
+		if (this.isUni) {
+			this.uniLoadShow(obj);
+		} else {
 
-	loadShow(obj) {
+		}
+	}
+	scLoadClose() {
+		if (this.isUni) {
+			this.uniLoadClose();
+		} else {
+
+		}
+	}
+	scMsgShow(title, duration) {
+		duration = duration || 2000
+		if (this.isUni) {
+			this.uniMsgShow(title, duration)
+		} else {
+
+		}
+	}
+	scMsgClose() {
+		if (this.isUni) {
+			this.uniMsgClose()
+		} else {
+
+		}
+	}
+	scStr(str) {
+		return ((str + '').replace(/[^a-zA-Z0-9]/g, '') || '').substr(-20)
+	}
+	// uni-app封装
+	uniApi(url, params, config) {
+		return new Promise((resolve, reject) => {
+			uni.request({
+				url: url,
+				data: params,
+				method: config.method || 'POST', // 'GET'||'POST'
+				header: config.header,
+				timeout: config.timeout || this.jsonConf.timeout,
+				success: (res) => {
+					resolve(res);
+				},
+				fail: (err) => {
+					reject(err)
+				}
+			});
+		})
+	}
+	uniLoadShow(obj) {
 		uni.showLoading(obj);
 	}
-	loadClose() {
+	uniLoadClose() {
 		uni.hideLoading();
 	}
-	msgShow(title, duration = 2000) {
+	uniMsgShow(title, duration) {
 		uni.showToast({
 			title,
 			duration,
 		});
 	}
-	msgClose() {
+	uniMsgClose() {
 		uni.hideToast();
 	}
 }
